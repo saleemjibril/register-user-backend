67ea1cd46e32211e27aa7ab8ff074fa407b0272f
saleem.jibril <saleem.jibril@netzence.com>
2024-03-18 10:38:53 +0100

    made port dynamic

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ import cors from 'cors';
 import './config/db.js';
 import allMyRoutes from './allMyRoutes.js';
 
+
 const app = express();
 
-
+const port = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json({
   limit: '200mb'
@@ -15,6 +16,6 @@ app.use("/api/v1", allMyRoutes);
 
 
 
-app.listen(4000, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log('Server started on port 4000');
 });
