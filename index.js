c2daa8605efafcb6a77bb1043fec5fe4e47e3805
saleem.jibril <saleemjibril5@gmail.com>
2024-12-29 12:21:50 +0100

    updated port configuration

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import allMyRoutes from './allMyRoutes.js';
 
 const app = express();
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 8000;
 app.use(cors());
 app.use(express.json({
   limit: '200mb'
@@ -16,6 +16,6 @@ app.use("/api/v1", allMyRoutes);
 
 
 
-app.listen(8000, () => {
+app.listen(port, () => {
   console.log('Server started on port 8000');
 });
