ce72704de76a733e3a2614144e6fc667ceaa9ba8
saleem.jibril <saleemjibril5@gmail.com>
2025-01-02 21:13:19 +0100

    exported express app as a function

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
 import express from 'express';
-import bodyParser from'body-parser';
+import bodyParser from 'body-parser';
 import cors from 'cors';
-import './config/db.js';
+import './config/db.js'; // Ensure this is correctly set up
 import allMyRoutes from './allMyRoutes.js';
-
 
 const app = express();
 
-const port = process.env.PORT || 8000;
+// Middleware
 app.use(cors());
-app.use(express.json({
-  limit: '200mb'
-}));
+app.use(express.json({ limit: '200mb' }));
 app.use("/api/v1", allMyRoutes);
 
-
-
-app.listen(port, () => {
-  console.log('Server started on port 8000');
-});
+// Export the Express app as a function
+export default app;
