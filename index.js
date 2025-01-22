e8947d2356ab9d05fbe07875a621b48ba750a905
saleem.jibril <saleemjibril5@gmail.com>
2025-01-22 22:48:10 +0100

    testing new index.js format

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 import express from 'express';
-import bodyParser from 'body-parser';
+import bodyParser from'body-parser';
 import cors from 'cors';
-import './config/db.js'; // Ensure this is correctly set up
+import './config/db.js';
 import allMyRoutes from './allMyRoutes.js';
+
 
 const app = express();
 
-// Middleware
+const port = process.env.PORT || 4000;
 app.use(cors());
-app.use(express.json({ limit: '200mb' }));
+app.use(express.json({
+  limit: '200mb'
+}));
 app.use("/api/v1", allMyRoutes);
 
-// Export the Express app as a function
-export default app;
+
+
+app.listen(process.env.PORT || 8000, () => {
+  console.log('Server started on port 4000');
+});
