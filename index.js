2d1b52e9fdbc4f73720b428807edede21e7b8b67
saleem.jibril <saleemjibril5@gmail.com>
2025-02-13 09:20:39 +0100

    added meal tracker

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
 import express from 'express';
-import bodyParser from'body-parser';
+import bodyParser from 'body-parser';
 import cors from 'cors';
-import './config/db.js';
+import './config/db.js'; // Ensure this is correctly set up
 import allMyRoutes from './allMyRoutes.js';
-
 
 const app = express();
 
-const port = process.env.PORT || 4000;
+// Middleware
 app.use(cors());
-app.use(express.json({
-  limit: '200mb'
-}));
+app.use(express.json({ limit: '200mb' }));
 app.use("/api/v1", allMyRoutes);
 
-
-
-app.listen(process.env.PORT || 8000, () => {
-  console.log('Server started on port 4000');
-});
+// Export the Express app as a function
+export default app;
